1892279eaea6637a2f2ca75cacd6d4a98f3f4d65
Gitkaonty <cpa_tahina@experts-cpa.com>
2026-01-29 12:24:12 +0300

    param back serveur

--- front/kaonti/config/axios.js
+++ front/kaonti/config/axios.js
@@ -1,9 +1,10 @@
 import axios from "axios";
-//const BASE_URL = 'http://localhost:5100';
-const BASE_URL = 'https://finance.inframad.com';
+const BASE_URL = 'http://localhost:5100';
+//const BASE_URL = 'https://finance.inframad.com';
 
 export default axios.create({
-    baseURL: BASE_URL
+    baseURL: BASE_URL,
+    withCredentials: true
 });
 
 export const axiosPrivate = axios.create({
